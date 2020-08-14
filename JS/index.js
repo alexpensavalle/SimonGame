b99e6374786fa37528696b670b15ba2a32046ecf
Alexander Pensavalle <alexanderpensavalle@Alexanders-MacBook-Pro.local>
2020-08-13 21:53:16 -0400

    8/13/20 - Up to Date and essentially MVP

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,7 @@
 var userAnswers = [];
 var SimonAnswers = [];
 var rounds = 0;
+const fullGame = 3;
 var interval;
 
 var addColor = function (arr) {
@@ -13,21 +14,24 @@ var flashLights = function (arr) {
   var i = 0;
 
   interval = setInterval(function () {
+  
+        $("#" + arr[i])
+          .fadeTo("fast", 0)
+          .fadeTo("fast", 1);
+
+        
+        $("#sound-" + arr[i])[0].play();
+
+        i++;
+
+        if (i >= (arr.length)) {
+          clearInterval(interval);
+        }
     
-    $("#" + arr[i])
-      .fadeTo("fast", 0)
-      .fadeTo("fast", 1);
-    
-    $("#sound-" + arr[i])[0].play();
-    
-    i++;
-    
-    if (i >= (arr.length)) {
-      clearInterval(interval);
-    }
 
   }, 1000);
-  console.log(interval);
+
+  console.log(arr[i]);
 };
 
 var increaseFontSize = function () {
@@ -38,15 +42,33 @@ var increaseFontSize = function () {
 }
 
 var decreaseFontSize = function () {
-var el = document.querySelectorAll(".menuButton");
-  for ( var i = 0; i < el.length; i ++ ) {
-      el[i].style.fontSize = "40px";
-  }
+  var el = document.querySelectorAll(".menuButton");
+    for ( var i = 0; i < el.length; i ++ ) {
+        el[i].style.fontSize = "40px";
+    }
+}
+
+var youWin = function () {
+    decreaseFontSize();
+    $(".menuButton").html("You Win!");
+    userAnswers = [];
+    SimonAnswers = [];
+    clearInterval(interval);
+    setTimeout(resetGame, 4000);
+    return true;
 }
 
 var updateRounds = function () {
-  rounds++;
-  $(".menuButton").html(rounds);
+          
+    if(rounds === fullGame) {
+      youWin();
+      console.log(youWin());
+    }
+    else {
+      rounds++;
+      $(".menuButton").html(rounds);
+    }
+
 };
 
 //Start Game:
@@ -61,56 +83,64 @@ $(".menuButton").click(function () {
 });
 
 var resetGame = function () {
-
   rounds = 0;
-  clearInterval(interval);//
+  clearInterval(interval);
   userAnswers = [];
   SimonAnswers = [];
+  increaseFontSize();
   playerTurn();
-  
 };
 
 var playerTurn = function () {
 
-  increaseFontSize();
-  updateRounds();
-  addColor(userAnswers);
-  flashLights(userAnswers);
+  if(rounds != fullGame){
+      updateRounds();
+    
+      addColor(userAnswers);
+      flashLights(userAnswers);
 
-  $(".button")
-    .off("click")
-    .on("click", function () {
-      $("#sound-" + $(this).attr("id"))[0].play();
-      SimonAnswers.push($(this).attr("id"));
+      $(".button")
+        .off("click")
+        .on("click", function () {
+          $("#sound-" + $(this).attr("id"))[0].play();
+          SimonAnswers.push($(this).attr("id"));
 
-      for (var i = 0; i < SimonAnswers.length; i++) {
-        //correct answer
-        if (JSON.stringify(userAnswers) === JSON.stringify(SimonAnswers)) {
-          SimonAnswers = [];
-          playerTurn();
-          break;
-        }//developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+          for (var i = 0; i < SimonAnswers.length; i++) {
+            //correct answer
+            if (JSON.stringify(userAnswers) === JSON.stringify(SimonAnswers)) {
+              SimonAnswers = [];
+              playerTurn();
+              break;
+            }//developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
 
-        //wrong answer
-        if (SimonAnswers[i] !== userAnswers[i]) {
-            
-            var el = document.querySelectorAll(".menuButton");
-            for ( var i = 0; i < el.length; i ++ ) 
-                el[i].style.fontSize = "40px";
-            
-            
-            $(".menuButton").html("Wrong!");
-            
-            setTimeout(
-              wrong = function () {
-                $(".menuButton").html("Start Over!");
+            //wrong answer
+            if (SimonAnswers[i] !== userAnswers[i]) {
+
+                var el = document.querySelectorAll(".menuButton");
+                for ( var i = 0; i < el.length; i ++ ) 
+                    el[i].style.fontSize = "40px";
+
+
+                $(".menuButton").html("Wrong!");
+
                 setTimeout(
-                  resetGame, 1000);
-              }, 
-              1000);
-        }
-     }   
-  });
+                  wrong = function () {
+                    $(".menuButton").html("Start Over!");
+                    setTimeout(
+                      resetGame, 1000);
+                  }, 
+                  1000);
+            }
+         }   
+      });
+
+  }
+  else{
+
+    youWin();
+    
+  }
+
 };
 
 
